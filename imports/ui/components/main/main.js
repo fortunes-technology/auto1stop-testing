b46cc612fab028778621f86268ff60e29bbaf4dc
Fortunes Technology <fortunes.technology@gmail.com>
2017-11-29 12:01:23 +0800

    Style Fix V1

--- imports/ui/components/main/main.js
+++ imports/ui/components/main/main.js
@@ -1,11 +1,65 @@
 import './main.html';
 import '../team/team.js';
 import '../brand/brand.js';
-import '../testimonial/testimonial.js'
+import '../testimonial/testimonial.js';
 
 Template.hello.onRendered(function helloOnRendered() {
     // counter starts at 0
     $(".owl-carousel").owlCarousel();
+
+	//$(document).ready(function() {
+	//Meteor.setTimeout(function () {
+	//	$("#rev_slider_2_1").show().revolution({
+	//		sliderType: "standard",
+	//		sliderLayout: "fullwidth",
+	//		dottedOverlay: "none",
+	//		delay: 1000,
+	//		navigation: {
+	//			keyboardNavigation: "off",
+	//			keyboard_direction: "horizontal",
+	//			mouseScrollNavigation: "off",
+	//			mouseScrollReverse: "default",
+	//			onHoverStop: "off",
+	//			bullets: {
+	//				enable: true,
+	//				hide_onmobile: false,
+	//				style: "hermes",
+	//				hide_onleave: false,
+	//				direction: "horizontal",
+	//				h_align: "center",
+	//				v_align: "bottom",
+	//				h_offset: 0,
+	//				v_offset: 50,
+	//				space: 10,
+	//				tmp: ''
+	//			}
+	//		},
+	//		visibilityLevels: [1240, 1024, 778, 480],
+	//		gridwidth: 1570,
+	//		gridheight: 1000,
+	//		lazyType: "none",
+	//		shadow: 0,
+	//		spinner: "spinner3",
+	//		stopLoop: "off",
+	//		stopAfterLoops: -1,
+	//		stopAtSlide: -1,
+	//		shuffle: "off",
+	//		autoHeight: "off",
+	//		disableProgressBar: "on",
+	//		hideThumbsOnMobile: "off",
+	//		hideSliderAtLimit: 0,
+	//		hideCaptionAtLimit: 0,
+	//		hideAllCaptionAtLilmit: 0,
+	//		debugMode: false,
+	//		fallbacks: {
+	//			simplifyAll: "off",
+	//			nextSlideOnWindowFocus: "off",
+	//			disableFocusListener: false,
+	//		}
+	//	});
+	////});
+	//}, 2000);
+
 });
 
 Template.hello.onCreated(function helloOnCreated() {
